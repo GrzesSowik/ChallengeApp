6e789dcf592836c304ec58721fb1b1fc4377bb64
GrzesSowik <gsowik@grzybki.waw.pl>
2024-04-10 21:02:56 +0200

    Zadnie domowe - dzień 8

---
+++ ChallengeApp/ChallengeApp.Tests/Type.Tests.cs
@@ -0,0 +1,50 @@
+﻿// Zadnie domowe - dzień 8
+
+using CHallengeApp;
+
+namespace ChallengeApp.Tests
+{
+
+    public class TypeTests
+    {
+        [Test]
+        public void IntegerTest()
+        {
+            int number1 = 2;
+            int number2 = 2;
+
+            Assert.AreEqual(number1, number2);
+        }
+
+        [Test]
+        public void floatTest()
+        {
+            float number1 = 13.48f;
+            float number2 = 13.48f;
+
+            Assert.AreEqual(number1, number2);
+        }
+        [Test]
+        public void StringTest()
+        {
+            string name1 = "Grześ";
+            string name2 = "Grześ";
+
+            Assert.AreEqual(name1, name2);
+        }
+
+        [Test]
+        public void ReferenceTest()
+        {
+            var emploee1 = new Emploee("Grześ", "Grzesiowski", 34);
+            var emploee2 = new Emploee("Grześ", "Grzesiowski", 34);
+
+            Assert.AreNotEqual(emploee1, emploee2);
+            Assert.AreEqual(emploee1.Name, emploee2.Name);
+        }
+        private Emploee GetUser(string name, string surname, int age)
+        {
+            return new Emploee(name, surname, age);
+        }
+    }
+}
