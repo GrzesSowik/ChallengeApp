390eb61809652c604050ba0be6508a1b58ec02aa
GrzesSowik <gsowik@grzybki.waw.pl>
2024-04-10 20:51:30 +0200

    Zadanie domowe - dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,1 +1,24 @@
-﻿Console.WriteLine("Hello Grześ");
+﻿// Zadanie domowe - dzień 4
+
+bool isFemale = true;
+var name = "Ewa";
+var age = 30;
+
+if (isFemale == true)
+{
+    if (age < 30)
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+    else if (age == 30 && name == "Ewa")
+    {
+        Console.WriteLine("Ewa, lat 30");
+    }
+}
+else
+{
+    if (age < 18)
+    {
+        Console.WriteLine("Męszczyzna ponieżej 18 lat");
+    }
+}
